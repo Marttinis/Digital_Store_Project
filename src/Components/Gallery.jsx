f569331640c6ba594526aa261025f35075230adb
Gabriel Martins <gabrielmartins23445@gmail.com>
2024-10-25 21:00:03 -0300

    criacao da section

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,58 +1,33 @@
 const Gallery = () => {
+    
+    //funções para passar pelas setas 
+  const goNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length)
+  }
+  const goPrev = () => {
+    setCurrentIndex((prevIndex) =>{
+      return prevIndex === 0 ? banners.length -1 : prevIndex - 1
+    })
+  }
+  const goToslide = (index) => {
+    setCurrentIndex(index)
+  }
+  //autoplay carousel home
+  useEffect(() => {
+    const interval = setInterval(() => {
+      goNext()
+    }, 4000)
+    return() => clearInterval(interval)
+  }, [])
+    
+    
+    
+    
     return (
-        <>
-        import { Carousel } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', padding: '20px', backgroundColor: "#F5F5F5" }}>
-                <div style={{ width: '100%' }}>
-                    <Carousel controls={false}>
-                        <Carousel.Item>
-                            <div style={{ display: 'flex', alignItems: 'center' }}>
-                                {/* Coluna do Texto */}
-                                <div style={{ flex: '1', textAlign: 'left' }}>
-                                    <p style={{ color: "orange", fontWeight: 'bold' }}>Melhores ofertas personalizadas</p>
-                                    <h1 style={{ color: "black", fontSize: '40px', fontWeight: 'bold' }}>Queima de estoque Nike 🔥</h1>
-                                    <p style={{ color: "gray", fontSize: '16px' }}>Consequat culpa exercitation mollit nisi exceptur do do tempor laboris eiusmod irure consectetur.</p>
-                                    <button
-                                        className="btn"
-                                        style={{
-                                            backgroundColor: '#C92071',
-                                            color: 'white',
-                                            borderColor: '#C92071',
-                                            width: '130px',
-                                            height: '45px',
-                                            fontWeight: 'bold',
-                                            fontSize: '16px'
-                                        }}
-                                        aria-label="Ver Ofertas"
-                                    >
-                                        Ver Ofertas
-                                    </button>
-                                </div>
-                                {/* Coluna da Imagem */}
-                                
-                                <div style={{ flex: '1', textAlign: 'right', margin:"0", padding:"0" }}>
-                                    <img
-                                        src="src\images\White-Sneakers-PNG-Clipart 1.png"
-                                        alt="Tênis Nike"
-                                        style={{ width: '700px' }}
-                                    />
-                                </div>
-
-                            </div>
-                        </Carousel.Item>
-                    </Carousel>
-                </div>
-            </div>
-
-
-
-
-        </>
+        <div>
+            
+        </div>
     );
 }
- 
+
 export default Gallery;
--- src/Components/Section.jsx
+++ src/Components/Section.jsx
@@ -1,8 +1,45 @@
-const Section = () => {
+
+
+// Define o estilo básico para o título e o link
+const styles = {
+    title: {
+        color: '#474747',
+        fontSize: '24px',
+
+
+    },
+    link: {
+        color: ' #C92071',
+        fontSize: '18px',
+        marginLeft: 'auto',
+        textDecoration: 'none',
+    },
+    container: (align) => ({
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: align === 'center' ? 'center' : 'flex-start',
+        gap: '8px',
+    }),
+};
+
+
+
+
+const Section = ({ title, titleAlign = 'left', link, children }) => {
     return (
-        <>
-        </>
+        <section>
+            <div style={styles.container(titleAlign)}>
+                <h2 style={styles.title}>{title}</h2>
+                {link && (
+                    <a href={link.href} style={styles.link}>
+                        {link.text}<i class="bi bi-arrow-right"></i>
+                    </a>
+
+                )}
+            </div>
+            <div>{children}</div>
+        </section>
     );
 }
- 
+
 export default Section;
